de7811e98d770f213968efb1922d0ad95ebff338
Kevin De Ritis <kevinderitis@MBP-de-Kevin.fibertel.com.ar>
2024-07-10 15:39:19 -0300

    Error in sheet

--- src/services/googleServices.js
+++ src/services/googleServices.js
@@ -16,40 +16,40 @@ const formatDate = (date) => {
 export const appendDataToSheet = async (spreadsheetId, values) => {
     const client = await auth.getClient();
     const range = 'Leads!A2';
-
-    await google.sheets('v4').spreadsheets.batchUpdate({
-        spreadsheetId,
-        resource: {
-            requests: [
-                {
-                    insertDimension: {
-                        range: {
-                            sheetId: 0,
-                            dimension: 'ROWS',
-                            startIndex: 1,
-                            endIndex: 2,
-                        },
-                        inheritFromBefore: false,
-                    },
-                },
-            ],
-        },
-        auth: client,
-    });
-
-    const formattedDate = formatDate(new Date());
-    console.log(formattedDate)
-    const request = {
-        spreadsheetId,
-        range,
-        valueInputOption: 'RAW',
-        resource: {
-            values: [[...values, formattedDate]],
-        },
-        auth: client,
-    };
-
     try {
+        await google.sheets('v4').spreadsheets.batchUpdate({
+            spreadsheetId,
+            resource: {
+                requests: [
+                    {
+                        insertDimension: {
+                            range: {
+                                sheetId: 0,
+                                dimension: 'ROWS',
+                                startIndex: 1,
+                                endIndex: 2,
+                            },
+                            inheritFromBefore: false,
+                        },
+                    },
+                ],
+            },
+            auth: client,
+        });
+
+        const formattedDate = formatDate(new Date());
+        console.log(formattedDate)
+        const request = {
+            spreadsheetId,
+            range,
+            valueInputOption: 'RAW',
+            resource: {
+                values: [[...values, formattedDate]],
+            },
+            auth: client,
+        };
+
+
         const response = (await google.sheets('v4').spreadsheets.values.update(request)).data;
         console.log(`Data appended to sheet: ${spreadsheetId}`);
     } catch (error) {
